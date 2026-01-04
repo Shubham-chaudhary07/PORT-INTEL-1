1fe444bc8b6d7fc9df0a5e44feeaa7aa73cb8754
Shubham Chaudhary <2006shubhamchaudhary@gmail.com>
2026-01-04 19:14:08 +0530

    third change

--- app.py
+++ app.py
@@ -66,5 +66,4 @@ def simulate_attack():
 
 # ================= ENTRY =================
 if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host="0.0.0.0", port=port)
+        app.run(debug=True)
